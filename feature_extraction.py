cc4bf8bf1a5eec6b395490ccc8a61353afe5902a
JonCS <jonathanjiaweisu@gmail.com>
2018-11-01 08:53:43 -0700

    added more features

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,15 +3,18 @@ import json_lines
 posts_features = []  # list of extracted features for each social media post
 
 
-def read_file():
+def extract_features():
     i = 0
     with json_lines.open('training_data/instances.jsonl') as reader:
         for obj in reader:
             features = []
             post_text = obj['postText'][0]
 
-            # feature - word length of post text
+            # feature – number of tokens
             features.append(len(post_text.split()))
+
+            # feature - word length of post text
+            features.append(len(post_text))
 
             # feature - whether the post starts with a number
             if post_text[0].isdigit():
@@ -19,12 +22,51 @@ def read_file():
             else:
                 features.append(0)
 
+            # feature - whether the post ends with a number
+            if post_text[-1].isdigit():
+                features.append(1)
+            else:
+                features.append(0)
+
+            # feature – whether the last word ends with a '?' mark
+            if post_text[-1] == '?':
+                features.append(1)
+            else:
+                features.append(0)
+
+            # feature – whether the last word ends with a '!' mark
+            if post_text[-1] == '!':
+                features.append(1)
+            else:
+                features.append(0)
+
+            # feature – whether the post contains the following key words
+            wwwwwwhw = ['who', 'what', 'when', 'where', 'why', 'how', 'which']
+            c = 0
+            for text in post_text.split():
+                if text.lower() in wwwwwwhw:
+                    c += 1
+                    #print(post_text)
+            features.append(c)
+
+            # feature – number of ',' in the post
+            c = 0
+            for text in post_text.split():
+                if ',' in text:
+                    c += 1
+                    #print(text)
+            features.append(c)
+
             posts_features.append(features)
 
             # used for testing smaller number of files - delete later
             i += 1
             if i > 100:
                 break
+
+
+def read_file():
+    extract_features()
 
     for features in posts_features:
         print(features)
