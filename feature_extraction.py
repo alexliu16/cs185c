f0063c8a59509f3c0fffb52cd2f38e45ee4aa070
JonCS <jonathanjiaweisu@gmail.com>
2018-11-10 15:40:37 -0800

    more features

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,4 +1,5 @@
 import json_lines
+from textblob import TextBlob
 
 posts_features = []  # list of extracted features for each social media post
 
@@ -9,6 +10,8 @@ def extract_features():
         for obj in reader:
             features = []
             post_text = obj['postText'][0]
+
+            blob = TextBlob(post_text)
 
             # feature – number of tokens
             features.append(len(post_text.split()))
@@ -42,6 +45,14 @@ def extract_features():
 
             # feature – whether the post contains the following key words
             wwwwwwhw = ['who', 'what', 'when', 'where', 'why', 'how', 'which']
+            if post_text.split()[0].lower() in wwwwwwhw:
+                features.append(1)
+                #print(post_text.split()[0])
+            else:
+                features.append(0)
+                #print(post_text.split()[0])
+
+            # feature – whether the post contains the following key words
             c = 0
             for text in post_text.split():
                 if text.lower() in wwwwwwhw:
@@ -57,6 +68,17 @@ def extract_features():
                     #print(text)
             features.append(c)
 
+            # feature – whether the post is all uppercase
+            if post_text.isupper():
+                #print(post_text)
+                features.append(1)
+
+            # feature – number of noun phrases in the post
+            c = 0
+            for phrase in blob.noun_phrases:
+                c += 1
+            features.append(c)
+
             posts_features.append(features)
 
             # used for testing smaller number of files - delete later
@@ -66,6 +88,7 @@ def extract_features():
 
 
 def read_file():
+    # extract features into post_features
     extract_features()
 
     for features in posts_features:
